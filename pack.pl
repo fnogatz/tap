d4b3c842df0b7eeb21aba35d97cb07e7a1488059
Michael Hendricks <michael@ndrix.org>
2013-07-12 14:13:12 -0600

    Bump version again
    Get the download URL right this time.

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(tap).
-version('0.1.1').
+version('0.1.2').
 title('Automated tests via Test Anything Protocol').
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
 home('http://packs.ndrix.com/tap/index.html').
-download('http://packs.ndrix.com/tap/tap-0.1.0.tgz').
+download('http://packs.ndrix.com/tap/tap-0.1.2.tgz').
