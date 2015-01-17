9a19858b498560626d7ab677e567bd4f94f083b0
Michael Hendricks <michael@ndrix.org>
2015-01-17 15:23:59 -0700

    Prepare v0.3.0 release

--- History.md
+++ History.md
@@ -1,3 +1,7 @@
+# v0.3.0 (2015-01-17)
+
+  * Add diag/2 for TAP diagnostic messages
+
 # v0.2.4 (2014-08-07)
 
   * Test for exceptions when failure expected
--- pack.pl
+++ pack.pl
@@ -1,10 +1,11 @@
 name(tap).
 
-version('0.2.4').
-download('http://packs.ndrix.com/tap/tap-0.2.4.tgz').
+version('0.3.0').
+download('https://github.com/mndrix/tap/archive/v0.3.0.zip').
 
 title('Automated tests via Test Anything Protocol').
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
-home('http://packs.ndrix.com/tap/index.html').
+
+home('https://github.com/mndrix/tap').
