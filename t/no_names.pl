546e9db655f5d27d6a7858342daedf26a0f79862
Michael Hendricks <michael@ndrix.org>
2015-04-10 14:14:20 -0600

    More test cases for anonymous tests
    Make sure that anonymous tests can call normal goals, not just operator
    goals.

--- t/no_names.pl
+++ t/no_names.pl
@@ -1,6 +1,12 @@
 % Tests whose name is the clause itself
+is_even(X) :-
+    0 is X mod 2.
+
 :- use_module(library(tap)).
 :- style_check(-no_effect).
+
+is_even(4).
+\+ is_even(5).
 
 % some examples taken nearly from ISO §8.4.1.4
 1.0 @< 1.
