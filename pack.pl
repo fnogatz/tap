6a476cbd5a16b55269e3c0c083240b2fc2097e4a
Falco Nogatz <fnogatz@gmail.com>
2019-08-28 10:54:54 +0200

    vv0.5.1

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(tap).
 
-version('0.5.0').
-download('https://github.com/fnogatz/tap/archive/v0.5.0.zip').
+version('0.5.1').
+download('https://github.com/fnogatz/tap/archive/v0.5.1.zip').
 
 title('Automated tests via Test Anything Protocol').
 author('Falco Nogatz','fnogatz@gmail.com').
