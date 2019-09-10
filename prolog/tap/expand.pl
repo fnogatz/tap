ca8d25bfb8ee4a807f4348a5b7d3f7415611da25
Falco Nogatz <fnogatz@gmail.com>
2019-09-10 22:25:12 +0200

    Add dynamic user:term_skips_tap_expansion/1 to define clauses that should not be expanded by library(tap)

--- prolog/tap/expand.pl
+++ prolog/tap/expand.pl
@@ -3,9 +3,12 @@
 :- use_module(library(tap/raw), [term_wants_tap_expansion/0]).
 
 :- dynamic test_case/1.
-user:term_expansion((Head:-_), _) :-
+:- dynamic user:term_skips_tap_expansion/1.
+user:term_expansion(Clause, _) :-
+    Clause = (Head :- _),
     % collect test cases with explicit names
     term_wants_tap_expansion,
+    \+ term_skips_tap_expansion(Clause),
     tap:assertz(test_case(Head)),
     fail.  % do no real expansion
 user:term_expansion(Clause, (Head :- Clause)) :-
@@ -13,6 +16,7 @@ user:term_expansion(Clause, (Head :- Clause)) :-
     term_wants_tap_expansion,
     \+ functor(Clause, :-, _),
     Clause \== end_of_file,
+    \+ term_skips_tap_expansion(Clause),
     format(atom(Head), "~w", [Clause]),
     writeln(Clause),
     tap:assertz(test_case(Head)).
